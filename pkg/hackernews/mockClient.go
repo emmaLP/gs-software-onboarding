07d5119c9299ce2f5035f4eb1c3241260a9bee22
Emma Roberts <emma.roberts@gymshark.com>
2021-11-26 16:01:51 +0000

    Re-implementing the mockClient

--- pkg/hackernews/mockClient.go
+++ pkg/hackernews/mockClient.go
@@ -1,33 +1,32 @@
 package hackernews
 
-//
-//import (
-//	"github.com/emmaLP/gs-software-onboarding/pkg/hackernews/model"
-//	"github.com/stretchr/testify/mock"
-//)
-//
-//type MockClient struct {
-//	mock.Mock
-//}
-//
-//func (m *MockClient) GetTopStories() ([]int, error) {
-//	args := m.Called()
-//
-//	idsArg, ok := args.Get(0).([]int)
-//	if !ok {
-//		return nil, nil
-//	}
-//
-//	return idsArg, args.Error(1)
-//}
-//
-//func (m *MockClient) GetItem(id int) (*model.Item, error) {
-//	args := m.Called(id)
-//
-//	itemArg, ok := args.Get(0).(*model.Item)
-//	if !ok {
-//		return nil, nil
-//	}
-//
-//	return itemArg, args.Error(1)
-//}
+import (
+	"github.com/emmaLP/gs-software-onboarding/pkg/hackernews/model"
+	"github.com/stretchr/testify/mock"
+)
+
+type MockClient struct {
+	mock.Mock
+}
+
+func (m *MockClient) GetTopStories() ([]int, error) {
+	args := m.Called()
+
+	idsArg, ok := args.Get(0).([]int)
+	if !ok {
+		return nil, nil
+	}
+
+	return idsArg, args.Error(1)
+}
+
+func (m *MockClient) GetItem(id int) (*model.Item, error) {
+	args := m.Called(id)
+
+	itemArg, ok := args.Get(0).(*model.Item)
+	if !ok {
+		return nil, nil
+	}
+
+	return itemArg, args.Error(1)
+}
