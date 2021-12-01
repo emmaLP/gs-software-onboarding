d6bd953c8dcb5eb23edebaecc869e3257f2410ac
Emma Roberts <emma.roberts@gymshark.com>
2021-12-01 10:39:57 +0000

    Changes to the service to support testing and adding tests

--- internal/consumer/service.go
+++ internal/consumer/service.go
@@ -3,17 +3,79 @@ package consumer
 import (
 	"context"
 	"fmt"
+	"log"
+
+	"github.com/emmaLP/gs-software-onboarding/internal/model"
 
 	"github.com/emmaLP/gs-software-onboarding/pkg/hackernews"
-
 	"go.uber.org/zap"
+	"golang.org/x/sync/errgroup"
 )
 
-func processStories(ctx context.Context, logger *zap.Logger, baseUrl string) error {
-	logger.Info("Processing stories")
-	client, err := hackernews.New(baseUrl, nil)
-	if err != nil {
-		fmt.Errorf("Unable to create HackerNew client: %w", err)
+type Service struct {
+	logger          *zap.Logger
+	numberOfWorkers int
+	hnClient        hackernews.Client
+}
+
+type Client interface {
+	processStories(ctx context.Context)
+}
+
+func NewService(logger *zap.Logger, config *model.ConsumerConfig, hnClient hackernews.Client) (*Service, error) {
+	if hnClient == nil {
+		var err error
+		hnClient, err = hackernews.New(config.BaseUrl, nil)
+		if err != nil {
+			return nil, fmt.Errorf("Unable to create HackerNew client: %w", err)
+		}
 	}
+	return &Service{
+		logger:          logger,
+		numberOfWorkers: config.NumberOfWorkers,
+		hnClient:        hnClient,
+	}, nil
+}
+
+func (s *Service) processStories(ctx context.Context) error {
+	s.logger.Info("Processing stories")
+
+	errGroup := new(errgroup.Group)
+	topStoriesChan := make(chan int)
+
+	for i := 0; i < s.numberOfWorkers; i++ {
+		errGroup.Go(func() error {
+			return s.saveItem(topStoriesChan)
+		})
+	}
+
+	s.logger.Info("Finished getting the items")
+
+	storyIds, err := s.hnClient.GetTopStories()
+	if err != nil {
+		return fmt.Errorf("Unable to retrieve the top stories. %w", err)
+	}
+	for _, id := range storyIds {
+		topStoriesChan <- id
+	}
+	close(topStoriesChan)
+
+	if err := errGroup.Wait(); err != nil {
+		return fmt.Errorf("An error occurred while saving the item. %w", err)
+	}
+	s.logger.Info("Finished processing stories")
+	return nil
+}
+
+func (s *Service) saveItem(topStoriesChan <-chan int) error {
+	for storyId := range topStoriesChan {
+		item, err := s.hnClient.GetItem(storyId)
+		if err != nil {
+			return fmt.Errorf("An error occurred when trying to fetch the item. %w", err)
+		} else if !item.Deleted && !item.Dead {
+			log.Println(item)
+		}
+	}
+	s.logger.Info("Finished looping through the channel for story ids")
 	return nil
 }
---
+++ internal/consumer/service_test.go
@@ -0,0 +1,60 @@
+package consumer
+
+import (
+	"context"
+	"testing"
+
+	"go.uber.org/zap"
+
+	"github.com/emmaLP/gs-software-onboarding/internal/model"
+	"github.com/emmaLP/gs-software-onboarding/pkg/hackernews"
+	hnModel "github.com/emmaLP/gs-software-onboarding/pkg/hackernews/model"
+)
+
+func TestProcessStories(t *testing.T) {
+	tests := map[string]struct {
+		hnMock         *hackernews.Mock
+		ids            []int
+		consumerConfig *model.ConsumerConfig
+		expectedMocks  func(t *testing.T, hnMock *hackernews.Mock)
+	}{
+		"One Item": {
+			hnMock: &hackernews.Mock{},
+			ids:    []int{1},
+			consumerConfig: &model.ConsumerConfig{
+				BaseUrl:         "test.com",
+				CronSchedule:    "",
+				NumberOfWorkers: 2,
+			},
+			expectedMocks: func(t *testing.T, hnMock *hackernews.Mock) {
+				hnMock.On("GetTopStories").Return([]int{1}, nil)
+				hnMock.On("GetItem", 1).Return(&hnModel.Item{ID: 1}, nil)
+			},
+		},
+	}
+	for testName, testConfig := range tests {
+		t.Run(testName, func(t *testing.T) {
+			if testConfig.expectedMocks != nil {
+				testConfig.expectedMocks(t, testConfig.hnMock)
+			}
+			logger, err := zap.NewProduction()
+			if err != nil {
+				panic(err)
+			}
+			service, err := NewService(logger, testConfig.consumerConfig, testConfig.hnMock)
+			if err != nil {
+				logger.Fatal("An unexpected err happened")
+				t.FailNow()
+			}
+			err = service.processStories(context.TODO())
+			if err != nil {
+				logger.Fatal("An unexpected err happened")
+				t.FailNow()
+			}
+
+			if testConfig.expectedMocks != nil {
+				testConfig.hnMock.AssertExpectations(t)
+			}
+		})
+	}
+}
