e8ae2449291ea2d4eede755ec7a56dd9d2059295
Emma Roberts <emma.roberts@gymshark.com>
2021-12-07 11:49:54 +0000

    Database functionality and writing unit and integration tests

--- internal/database/client.go
+++ internal/database/client.go
@@ -3,6 +3,9 @@ package database
 import (
 	"context"
 	"fmt"
+	"strings"
+
+	"go.mongodb.org/mongo-driver/bson"
 
 	"go.uber.org/zap"
 
@@ -20,32 +23,49 @@ type Database interface {
 }
 
 type database struct {
-	mongoClient *mongo.Client
-	context     context.Context
-	logger      *zap.Logger
+	mongoClient  *mongo.Client
+	context      context.Context
+	logger       *zap.Logger
+	databaseName string
 }
 
 const mongoUriTemplate = "mongodb://%s:%s"
 
 func New(ctx context.Context, logger *zap.Logger, config *model.DatabaseConfig) (*database, error) {
 	mongoUri := fmt.Sprintf(mongoUriTemplate, config.Host, config.Port)
-	credentials := options.Credential{
-		Username: config.Username,
-		Password: config.Password,
+
+	opts := options.Client().ApplyURI(mongoUri)
+	if strings.TrimSpace(config.Username) != "" && strings.TrimSpace(config.Password) != "" {
+		credentials := options.Credential{
+			Username: config.Username,
+			Password: config.Password,
+		}
+		opts = opts.SetAuth(credentials)
 	}
-	opts := options.Client().ApplyURI(mongoUri).SetAuth(credentials)
 	client, err := mongo.Connect(ctx, opts)
 	if err != nil {
 		return nil, fmt.Errorf("An error occurred when trying to connect to mongo. %w", err)
 	}
 	return &database{
-		mongoClient: client,
-		context:     ctx,
-		logger:      logger,
+		mongoClient:  client,
+		context:      ctx,
+		logger:       logger,
+		databaseName: config.Name,
 	}, nil
 }
 
-func (d *database) SaveItem(item hnModel.Item) error {
+func (d *database) SaveItem(item *hnModel.Item) error {
+	collection := d.getCollection("items")
+	opts := options.Update().SetUpsert(true)
+
+	update := bson.M{
+		"$set": item,
+	}
+	_, err := collection.UpdateOne(d.context, bson.M{"id": item.ID}, update, opts)
+	if err != nil {
+		return fmt.Errorf("Unable to save item. %w", err)
+	}
+	d.logger.Info("Item %d saved successfully", zap.Int("ID", item.ID))
 	return nil
 }
 
@@ -55,4 +75,9 @@ func (d *database) CloseConnection() {
 	if err != nil {
 		d.logger.Error("Failed to close connection to database", zap.Error(err))
 	}
+}
+
+func (d *database) getCollection(collectionName string) *mongo.Collection {
+	database := d.mongoClient.Database(d.databaseName)
+	return database.Collection(collectionName)
 }
--- internal/database/client_test.go
+++ internal/database/client_test.go
@@ -4,8 +4,6 @@ import (
 	"context"
 	"testing"
 
-	hnModel "github.com/emmaLP/gs-software-onboarding/pkg/hackernews/model"
-
 	"github.com/emmaLP/gs-software-onboarding/internal/model"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -27,22 +25,4 @@ func TestCloseConnection(t *testing.T) {
 	require.NoError(t, err)
 
 	client.CloseConnection()
-}
-
-func TestSaveItem(t *testing.T) {
-	tests := map[string]struct {
-		config         *model.DatabaseConfig
-		expectedResult *hnModel.Item
-		expectedErr    string
-	}{}
-
-	for testName, testConfig := range tests {
-		t.Run(testName, func(t *testing.T) {
-			logger, err := zap.NewProduction()
-			require.NoError(t, err)
-			client, err := New(context.TODO(), logger, testConfig.config)
-			require.NoError(t, err)
-			assert.NotNil(t, client)
-		})
-	}
 }
