b67cf634659d4077b62ef15b4c5ec842edd0c0d6
Emma Roberts <emma.roberts@gymshark.com>
2022-01-06 09:55:56 +0000

    Converting the api to communicate using the grpc service instead of having direct access to the cache and database

---
+++ internal/grpc/mockClient.go
@@ -0,0 +1,32 @@
+package grpc
+
+import (
+	"context"
+
+	"github.com/emmaLP/gs-software-onboarding/pkg/common/model"
+	"github.com/stretchr/testify/mock"
+)
+
+type Mock struct {
+	mock.Mock
+}
+
+func (m *Mock) ListAll(ctx context.Context) ([]model.Item, error) {
+	return handleCall(m.Called(ctx))
+}
+
+func (m *Mock) ListStories(ctx context.Context) ([]model.Item, error) {
+	return handleCall(m.Called(ctx))
+}
+
+func (m *Mock) ListJobs(ctx context.Context) ([]model.Item, error) {
+	return handleCall(m.Called(ctx))
+}
+
+func handleCall(args mock.Arguments) ([]model.Item, error) {
+	itemsArgs, ok := args.Get(0).([]model.Item)
+	if !ok {
+		return nil, args.Error(1)
+	}
+	return itemsArgs, args.Error(1)
+}
